e0563a8de178c9e8b35bc6916f449f150d85da12
theyatokami <estevanbiauloyer@gmail.com>
2023-06-22 10:52:08 +0200

    Update Objetct_oriented_programming.py

--- object_oriented_programming/Objetct_oriented_programming.py
+++ object_oriented_programming/Objetct_oriented_programming.py
@@ -8,10 +8,31 @@ class MyClass:
         print("Hello, " + self.name + "!")
 
 # Create an object of the class
-obj = MyClass("Alice")
+obj = MyClass("User")
 
 # Access object's attributes
 print(obj.name)
 
 # Call object's methods
 obj.say_hello()
+
+# Base class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print("An animal speaks.")
+
+# Derived class
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks.")
+
+# Create objects of the classes
+animal = Animal("Generic Animal")
+dog = Dog("Bobby")
+
+# Call methods
+animal.speak()
+dog.speak()
