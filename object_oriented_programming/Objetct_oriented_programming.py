60afd9c675186283917c216d0926d03c21a3531d
theyatokami <estevanbiauloyer@gmail.com>
2023-06-22 16:08:33 +0200

    rust addition

--- object_oriented_programming/Objetct_oriented_programming.py
+++ object_oriented_programming/Objetct_oriented_programming.py
@@ -70,19 +70,16 @@ circle = Circle()
 
 print(rectangle.calculate_area(4, 5))
 print(circle.calculate_area(3))
-class Animal:
-    pass
 
-class Dog(Animal):
-    pass
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
 
-animal = Animal()
-dog = Dog()
-
-print(isinstance(animal, Animal))  # True
-print(isinstance(dog, Animal))  # True
-print(isinstance(animal, Dog))  # False
-print(isinstance(dog, Dog))  # True
-
-print(issubclass(Dog, Animal))  # True
-print(issubclass(Animal, Dog))  # False
+@my_decorator
+def say_hello():
+    print("Hello!")
+    
+say_hello()
---
+++ rust basics/my_project/target/debug/deps/my_project.d
@@ -0,0 +1,5 @@
+C:\Users\223107527\Documents\GitHub\work\rust basics\my_project\target\debug\deps\my_project.exe: src\main.rs
+
+C:\Users\223107527\Documents\GitHub\work\rust basics\my_project\target\debug\deps\my_project.d: src\main.rs
+
+src\main.rs:
