738b9fde033a07bc3244a91c5e41f11514f6fe83
theyatokami <estevanbiauloyer@gmail.com>
2023-06-22 11:53:36 +0200

    Update Objetct_oriented_programming.py

--- object_oriented_programming/Objetct_oriented_programming.py
+++ object_oriented_programming/Objetct_oriented_programming.py
@@ -36,3 +36,53 @@ dog = Dog("Bobby")
 # Call methods
 animal.speak()
 dog.speak()
+
+class BankAccount:
+    def __init__(self, account_number):
+        self.__account_number = account_number  # Private attribute
+
+    def get_account_number(self):
+        return self.__account_number  # Public method to access private attribute
+
+# Create an object
+account = BankAccount("1234567890")
+
+# Access attribute through a public method
+print(account.get_account_number())
+
+# Base class
+class Shape:
+    def calculate_area(self):
+        pass  # Abstract method
+
+# Derived classes
+class Rectangle(Shape):
+    def calculate_area(self, length, width):
+        return length * width
+
+class Circle(Shape):
+    def calculate_area(self, radius):
+        return 3.14 * radius * radius
+
+# Create objects and call methods
+rectangle = Rectangle()
+circle = Circle()
+
+print(rectangle.calculate_area(4, 5))
+print(circle.calculate_area(3))
+class Animal:
+    pass
+
+class Dog(Animal):
+    pass
+
+animal = Animal()
+dog = Dog()
+
+print(isinstance(animal, Animal))  # True
+print(isinstance(dog, Animal))  # True
+print(isinstance(animal, Dog))  # False
+print(isinstance(dog, Dog))  # True
+
+print(issubclass(Dog, Animal))  # True
+print(issubclass(Animal, Dog))  # False
